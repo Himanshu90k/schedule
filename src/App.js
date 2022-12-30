7b79b2c70fd3c42e3b74b68d498e6fde0537f211
Himanshu Rawat <himanshurawat90k@gmail.com>
2022-12-30 21:09:17 +0530

    ui completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import {useState} from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
+
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const[tasks, setTasks] = useState([
     {
@@ -25,6 +28,14 @@ function App() {
     },
 ])
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor((Math.random() * 10000) + 1)
+
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -37,7 +48,8 @@ function App() {
 
   return (
     <div className='container'>
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? 
       (<Tasks  tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) 
       : 'No Tasks to show'
